2407d27ae6f65725870a4bdb6c2a932966e360db
Tibin Thomas <tibinthomas@outlook.com>
2017-07-04 09:52:22 +0530

    passing custom headers if needed.
    Note: Make sure to import { Headers } from '@angular/http';

--- src/app/server-service.service.ts
+++ src/app/server-service.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Headers } from '@angular/http';
+
 
 @Injectable()
 export class ServerServiceService {
 
+  
   constructor(private http: Http) { }
-
+ 
   saveDataService(server: any[]) {
-    return this.http.post('https://angular-server-database.firebaseio.com/serverData.json', server);
+    const headers: Headers = new Headers({'Content-Type': 'application/json'});
+    return this.http.post('https://angular-server-database.firebaseio.com/serverData.json',
+       server,
+      { headers: headers });
   }
 }
